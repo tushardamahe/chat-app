5290e446b560d78c085947249b2b36293adc8928
Tushar Damahe <qubetushar@gmail.com>
2023-03-20 20:40:46 +0530

    added sign-in page and sign with facebook/google

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router';
 
-const PrivateRoute = (children, ...routeProps) => {
+const PrivateRoute = ({ children, ...routeProps }) => {
   const profile = false;
 
   if (!profile) {
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
   apiKey: 'AIzaSyCgweBIvUElnyoLNZl6SiEX4flU6vnyaE4',
@@ -12,3 +14,6 @@ const config = {
 };
 
 const app = firebase.initializeApp(config);
+
+export const auth = app.auth();
+export const database = app.database();
