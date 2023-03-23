4e6e96daab1fa8a2af9893c36ef8f45216aac8bc
Tushar Damahe <qubetushar@gmail.com>
2023-03-23 17:42:24 +0530

    created layout for chat page

--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router';
+import { Loader } from 'rsuite';
+import ChatBottom from '../../components/chat-window/bottom';
+import Messages from '../../components/chat-window/messages';
+import ChatTop from '../../components/chat-window/top';
+import { useRooms } from '../../context/rooms.context';
 
 const Chat = () => {
-  return (
-    <div>Hello</div>
-  )
-}
+  const { chatId } = useParams();
 
-export default Chat
+  const rooms = useRooms();
+
+  if (!rooms) {
+    return <Loader center vertical size="md" content="Loading" speed="slow" />;
+  }
+
+  const currentRoom = rooms.find(room => room.id === chatId);
+
+  if (!currentRoom) {
+    return <h6 className="text-center mt-page">Chat {chatId} Not Found!</h6>;
+  }
+
+  return (
+    <>
+      <div className="chat-top">
+        <ChatTop />
+      </div>
+      <div className="chat-middle">
+        <Messages />
+      </div>
+      <div className="chat-bottom">
+        <ChatBottom />
+      </div>
+    </>
+  );
+};
+
+export default Chat;
