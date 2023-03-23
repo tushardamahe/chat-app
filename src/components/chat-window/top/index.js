cb862dde27ba5f3f0d96c165b33cb02ef3f20951
Tushar Damahe <qubetushar@gmail.com>
2023-03-23 20:20:20 +0530

    started to build chat top, created room description modal

--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -1,9 +1,41 @@
 import React, { memo } from 'react';
+import { Link } from 'react-router-dom';
+import { ButtonToolbar, Icon } from 'rsuite';
 import { useCurrentRoom } from '../../../context/current-room.context';
+import { useMediaQuery } from '../../../misc/custom-hooks';
+import RoomInfoBtnModal from './RoomInfoBtnModal';
 
 const ChatTop = () => {
   const name = useCurrentRoom(v => v.name);
-  return <div>{name}</div>;
+  const isMobile = useMediaQuery('(max-width: 992px)');
+
+  return (
+    <div>
+      <div className="d-flex justify-content-between align-items-center">
+        <h4>
+          <Icon
+            componentClass={Link}
+            to="/"
+            icon="arrow-circle-left"
+            size="2x"
+            className={
+              isMobile
+                ? 'd-inline-block p-0 mr-2 text-blue link-unstyled'
+                : 'd-none'
+            }
+          />
+          <span> {name}</span>
+        </h4>
+
+        <ButtonToolbar className="ws-nowrap">Todo</ButtonToolbar>
+      </div>
+
+      <div className="d-flex justify-content-between align-items-center">
+        <span>Todo</span>
+        <RoomInfoBtnModal />
+      </div>
+    </div>
+  );
 };
 
 export default memo(ChatTop);
