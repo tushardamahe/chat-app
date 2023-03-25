6e6495680e2eb4058677bfe413283f7bfd3a98c5
Tushar Damahe <qubetushar@gmail.com>
2023-03-25 11:37:08 +0530

    handleDelete message operation created

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -81,6 +81,40 @@ const Messages = () => {
     Alert.info(alertMsg, 4000);
   }, []);
 
+  const handleDelete = useCallback(
+    async msgId => {
+      // eslint-disable-next-line no-alert
+      if (!window.confirm('Delete this message?')) {
+        return;
+      }
+
+      const isLast = messages[messages.length - 1].id === msgId;
+
+      const updates = {};
+
+      updates[`/messages/${msgId}`] = null;
+
+      if (isLast && messages.length > 1) {
+        updates[`/rooms/${chatId}/lastMessage`] = {
+          ...messages[messages.length - 2],
+          msgId: messages[messages.length - 2].id,
+        };
+
+        if (isLast && messages.length === 1) {
+          updates[`/rooms/${chatId}/lastMessage`] = null;
+        }
+
+        try {
+          await database.ref().update(updates);
+          Alert.success('Message has been deleted');
+        } catch (err) {
+          Alert.error(err.message, 4000);
+        }
+      }
+    },
+    [chatId, messages]
+  );
+
   return (
     <ul className="msg-list custom-scroll">
       {isChatEmpty && <li>No Messages Yet.</li>}
@@ -91,6 +125,7 @@ const Messages = () => {
             message={msg}
             handleAdmin={handleAdmin}
             handleLike={handleLike}
+            handleDelete={handleDelete}
           />
         ))}
     </ul>
