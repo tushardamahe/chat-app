4b9506c566398edfeb4de1abb93b9fb31c7d9a99
Tushar Damahe <qubetushar@gmail.com>
2023-03-25 12:25:06 +0530

    created attachment buttom with uploader component

--- src/components/chat-window/bottom/index.js
+++ src/components/chat-window/bottom/index.js
@@ -4,6 +4,7 @@ import firebase from 'firebase/app';
 import { useParams } from 'react-router';
 import { useProfile } from '../../../context/profile.context';
 import { database } from '../../../misc/firebase';
+import AttachmentBtnModal from './AttachmentBtnModal';
 
 function assembleMessage(profile, chatId) {
   return {
@@ -69,6 +70,7 @@ const ChatBottom = () => {
   return (
     <div>
       <InputGroup>
+        <AttachmentBtnModal />
         <Input
           placeholder="Write a new message here..."
           value={input}
